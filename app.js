7cf3c63659c42d14abfe3d81fff032a03624da64
Boris0179 <boris.cabrera.f@gmail.com>
2025-02-02 02:24:18 -0300

    prueba de list

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
 console.log(amigos);
+//Funcion que lista amigos
+
 function agregarAmigo(){
-    let amigo =document.getElementById("amigo").value;
-    console.log(amigo);
+    let amigo =document.getElementById("amigo").value.toLocaleLowerCase();
     if (amigo.length == 0 )
     {
-        alert("El campo esta Vacio");
+        alert('Por favor, inserte un nombre');
     } else{
         amigos.push(amigo);
         console.log(amigos);
+        limpiarCaja();
     }
-
+    let listaHtml= document.getElementById('listaAmigos');
+    listaHtml.innerHTML='';
+    for (let index = 0; index < amigos.length; index++) {
+        const element = amigos[index];
+        listaHtml.innerHTML += `<li>${element}</li>`;
+       
+    }
     return;
+}
+
+function limpiarCaja() {
+    document.querySelector('#amigo').value = '';
 }
