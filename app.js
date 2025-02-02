d40a5345c03396f7f5b747edb9842e9a8e6d13f1
Boris0179 <boris.cabrera.f@gmail.com>
2025-02-02 01:39:52 -0300

    Crear arreglo Cargar lista

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+console.log(amigos);
+function agregarAmigo(){
+    let aaa =document.getElementById("amigo").value;
+    console.log(aaa);
+    amigos.push(document.getElementById("amigo").value);
+    return;
+}
